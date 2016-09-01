531a940b5f7d08c3df15864fa4566ecfb16d3764
Matt Diephouse <matt@diephouse.com>
2016-09-01 11:33:22 -0400

    Create the Xcode project

---
+++ Sources/Logician.h
@@ -0,0 +1,19 @@
+//
+//  Logician.h
+//  Logician
+//
+//  Created by Matt Diephouse on 9/1/16.
+//  Copyright © 2016 Matt Diephouse. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Logician.
+FOUNDATION_EXPORT double LogicianVersionNumber;
+
+//! Project version string for Logician.
+FOUNDATION_EXPORT const unsigned char LogicianVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Logician/PublicHeader.h>
+
+
