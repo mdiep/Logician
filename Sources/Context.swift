7b99245ff74bc3aab46cdedf4fda4b58db952f45
Matt Diephouse <matt@diephouse.com>
2016-09-01 23:36:18 -0400

    Add a Context data structure to store associations

---
+++ Sources/Context.swift
@@ -0,0 +1,67 @@
+//
+//  Context.swift
+//  Logician
+//
+//  Created by Matt Diephouse on 9/1/16.
+//  Copyright © 2016 Matt Diephouse. All rights reserved.
+//
+
+private class Node<Key: Hashable, Value> {
+    var keys: Set<Key>
+    var value: Value
+    
+    init(keys: Set<Key>, value: Value) {
+        self.keys = keys
+        self.value = value
+    }
+}
+
+internal struct Context<Key: Hashable, Value> {
+    private var values: [Key: Node<Key, Value>] = [:]
+    
+    subscript(_ key: Key) -> Value? {
+        get {
+            return values[key]?.value
+        }
+        set(newValue) {
+            if let newValue = newValue {
+                updateValue(newValue, forKey: key)
+            } else {
+                removeValue(forKey: key)
+            }
+        }
+    }
+    
+    mutating func merge(_ key1: Key, _ key2: Key, combine: (Value?, Value?) -> Value) {
+        let node1 = values[key1]
+        let node2 = values[key2]
+        let newValue = combine(node1?.value, node2?.value)
+        let allKeys = (node1?.keys ?? Set()).union(node2?.keys ?? Set()).union([ key1, key2 ])
+        let node = Node(keys: allKeys, value: newValue)
+        for key in allKeys {
+            values[key] = node
+        }
+    }
+    
+    mutating func removeValue(forKey key: Key) {
+        values.removeValue(forKey: key)
+    }
+    
+    @discardableResult
+    mutating func updateValue(_ newValue: Value, forKey key: Key) -> Value? {
+        return updateValue(forKey: key) { _ in newValue }
+    }
+    
+    @discardableResult
+    mutating func updateValue(forKey key: Key, transform: (Value?) -> Value) -> Value? {
+        let node = values[key]
+        let oldValue = node?.value
+        let newValue = transform(oldValue)
+        if let node = node {
+            node.value = newValue
+        } else {
+            values[key] = Node(keys: Set([ key ]), value: newValue)
+        }
+        return oldValue
+    }
+}
---
+++ Tests/ContextTests.swift
@@ -0,0 +1,71 @@
+//
+//  ContextTests.swift
+//  Logician
+//
+//  Created by Matt Diephouse on 9/1/16.
+//  Copyright © 2016 Matt Diephouse. All rights reserved.
+//
+
+import XCTest
+@testable import Logician
+
+private let key1 = "a"
+private let key2 = "b"
+private let key3 = "c"
+
+class ContextTests: XCTestCase {
+    func testUpdateValueForKey() {
+        var context = Context<String, Int>()
+        
+        XCTAssertNil(context[key1])
+        context.updateValue(8, forKey: key1)
+        XCTAssertEqual(context[key1], 8)
+    }
+    
+    func testRemoveValueForKey() {
+        var context = Context<String, Int>()
+        context.updateValue(8, forKey: key1)
+        
+        XCTAssertEqual(context[key1], 8)
+        context.removeValue(forKey: key1)
+        XCTAssertNil(context[key1])
+    }
+    
+    func testUpdateValueForKeyTransform() {
+        var context = Context<String, Int>()
+        
+        context.updateValue(forKey: key1) { value in
+            XCTAssertNil(value)
+            return 7
+        }
+        XCTAssertEqual(context[key1], 7)
+        
+        context.updateValue(forKey: key1) { value in
+            XCTAssertEqual(value, 7)
+            return 3
+        }
+        XCTAssertEqual(context[key1], 3)
+    }
+    
+    func testMerge() {
+        var context = Context<String, Int>()
+        
+        context.merge(key1, key2) { _, _ in 3 }
+        XCTAssertEqual(context[key1], 3)
+        XCTAssertEqual(context[key2], 3)
+        
+        context[key1] = 4
+        XCTAssertEqual(context[key1], 4)
+        XCTAssertEqual(context[key2], 4)
+        
+        context.merge(key2, key3) { _, _ in 5 }
+        XCTAssertEqual(context[key1], 5)
+        XCTAssertEqual(context[key2], 5)
+        XCTAssertEqual(context[key3], 5)
+        
+        context[key1] = 6
+        XCTAssertEqual(context[key1], 6)
+        XCTAssertEqual(context[key2], 6)
+        XCTAssertEqual(context[key3], 6)
+    }
+}
