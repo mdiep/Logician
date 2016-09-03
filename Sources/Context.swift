5b2a9dc20072d5a3914df20e28accc716e03a673
Matt Diephouse <matt@diephouse.com>
2016-09-03 14:16:36 -0500

    Let Context block parameters throw

--- Sources/Context.swift
+++ Sources/Context.swift
@@ -28,10 +28,10 @@ internal struct Context<Key: Hashable, Value> {
         }
     }
     
-    mutating func merge(_ key1: Key, _ key2: Key, combine: (Value?, Value?) -> Value) {
+    mutating func merge(_ key1: Key, _ key2: Key, combine: (Value?, Value?) throws -> Value) rethrows {
         let node1 = values[key1]
         let node2 = values[key2]
-        let newValue = combine(node1?.value, node2?.value)
+        let newValue = try combine(node1?.value, node2?.value)
         let allKeys = (node1?.keys ?? Set()).union(node2?.keys ?? Set()).union([ key1, key2 ])
         let node = Node(keys: allKeys, value: newValue)
         for key in allKeys {
@@ -49,10 +49,10 @@ internal struct Context<Key: Hashable, Value> {
     }
     
     @discardableResult
-    mutating func updateValue(forKey key: Key, transform: (Value?) -> Value?) -> Value? {
+    mutating func updateValue(forKey key: Key, transform: (Value?) throws -> Value?) rethrows -> Value? {
         let node = values[key]
         let oldValue = node?.value
-        let newValue = transform(oldValue)
+        let newValue = try transform(oldValue)
         if let node = node {
             node.value = newValue
         } else {
