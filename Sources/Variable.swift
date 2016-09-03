b79a35ce38ee7a05b236a6a52c63bbfafd52ba9a
Matt Diephouse <matt@diephouse.com>
2016-09-03 09:49:45 -0400

    Document Variable

--- Sources/Variable.swift
+++ Sources/Variable.swift
@@ -8,24 +8,31 @@
 
 import Foundation
 
+/// A class used to provide identity to `Variable`s.
 private class Identity { }
 
+/// An unknown value in a logic problem.
 public struct Variable<Value> {
+    /// The identity of the variable.
     fileprivate let identity: Identity
     
+    /// Create a new variable.
     public init() {
         self.init(identity: Identity())
     }
     
+    /// Create a variable with an existing `Identity`.
     private init(identity: Identity) {
         self.identity = identity
     }
     
+    /// A type-erased version of the variable.
     internal var erased: Variable<Any> {
         return Variable<Any>(identity: identity)
     }
 }
 
+/// Test whether the `Variable`s have the same identity.
 internal func == <Left, Right>(lhs: Variable<Left>, rhs: Variable<Right>) -> Bool {
     return lhs.identity === rhs.identity
 }
