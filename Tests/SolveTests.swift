e8c0fd1de5498400827cddd6e98b8fb9fb98b125
Matt Diephouse <matt@diephouse.com>
2016-09-06 22:26:24 -0400

    Also verify that Optionals are dropped

--- Tests/SolveTests.swift
+++ Tests/SolveTests.swift
@@ -12,9 +12,11 @@ import Logician
 class SolveTests: XCTestCase {
     func testSolve() {
         let xs: AnyIterator<Int> = solve { x in
+            let y = Variable<Int>()
             return any(
                 x == 5,
                 x == 6,
+                x == y,
                 x == 7
             )
         }
