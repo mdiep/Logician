4456fe8468ac0d2d80f434243503b54bdc0acb28
Matt Diephouse <matt@diephouse.com>
2016-09-16 22:18:10 -0400

    Add a class for information about unified variables

--- Sources/State.swift
+++ Sources/State.swift
@@ -14,7 +14,19 @@ public enum Error: Swift.Error {
 
 /// A partial or complete solution to a logic problem.
 public struct State {
-    private var context = Context<AnyVariable, Any>()
+    /// Information about a set of unified variables.
+    private class Info {
+        /// The value of the variables, if any.
+        var value: Any?
+        
+        /// The value of the variables, if any, casted to `Value`.
+        func value<Value>(_ type: Value.Type) -> Value? {
+            return value.map { $0 as! Value }
+        }
+    }
+    
+    /// The data backing the state.
+    private var context = Context<AnyVariable, Info>()
     
     /// Look up the value of a variable.
     ///
@@ -24,7 +36,7 @@ public struct State {
     /// - returns: The value of the variable, or `nil` if the value is unknown
     ///            or the variable isn't in the `State`.
     public func value<Value>(of variable: Variable<Value>) -> Value? {
-        return context[variable.erased].map { $0 as! Value }
+        return context[variable.erased]?.value.map { $0 as! Value }
     }
     
     /// Unify a variable with a value.
@@ -35,12 +47,14 @@ public struct State {
     ///
     /// - note: `throws` if `variable` already has a different value.
     public mutating func unify<Value: Equatable>(_ variable: Variable<Value>, _ value: Value) throws {
-        try context.updateValue(forKey: variable.erased) { oldValue in
-            if let oldValue = oldValue.map({ $0 as! Value }), oldValue != value {
+        try context.updateValue(forKey: variable.erased) { oldInfo in
+            if let oldValue = oldInfo?.value(Value.self), oldValue != value {
                 throw Error.UnificationError
             }
             
-            return value
+            let newInfo = oldInfo ?? Info()
+            newInfo.value = value
+            return newInfo
         }
     }
     
@@ -68,7 +82,7 @@ public struct State {
     /// - note: `throws` if the variables have existing, inequal values.
     public mutating func unify<Value: Equatable>(_ lhs: Variable<Value>, _ rhs: Variable<Value>) throws {
         try context.merge(lhs.erased, rhs.erased) { lhs, rhs in
-            if let lhs = lhs as! Value?, let rhs = rhs as! Value?, lhs != rhs {
+            if let lhs = lhs?.value(Value.self), let rhs = rhs?.value(Value.self), lhs != rhs {
                 throw Error.UnificationError
             }
             
