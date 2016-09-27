64d7977e88eb432df2771a865967641aaf96e112
Matt Diephouse <matt@diephouse.com>
2016-09-26 23:05:40 -0400

    Implement bimap

--- Sources/Variable.swift
+++ Sources/Variable.swift
@@ -16,13 +16,63 @@ public protocol VariableProtocol: PropertyProtocol {
     var variable: Variable<Value> { get }
 }
 
+/// Type-erased information about a bijection created with `bimap`.
+internal struct Bijection {
+    /// A function that takes a state and a value and attempts to return a state
+    /// where a variable is unified with that value.
+    typealias Function = (State, Any) throws -> State
+    
+    /// The variable that was created with `bimap`.
+    var x: AnyVariable
+    
+    /// The variable that `bimap` was called on.
+    var y: AnyVariable
+    
+    /// A function that takes state and a value of `y`, and attempts to unify
+    /// `x` with the corresponding value.
+    var toX: Function
+    
+    /// A function that takes state and a value of `x`, and attempts to unify
+    /// `y` with the corresponding value.
+    var toY: Function
+}
+
+extension VariableProtocol where Value: Equatable {
+    /// Create a new variable that's related to this one by a transformation.
+    public func bimap<NewValue: Equatable>(forward: @escaping (Value) -> NewValue, backward: @escaping (NewValue) -> Value) -> Variable<NewValue> {
+        let variable = AnyVariable()
+        let bijection = Bijection(
+            x: variable,
+            y: self.variable.erased,
+            toX: { state, value in
+                return try state.unifying(variable, forward(value as! Value))
+            },
+            toY: { state, value in
+                return try state.unifying(self.variable, backward(value as! NewValue))
+            }
+        )
+        return Variable<NewValue>(variable, bijection: bijection)
+    }
+}
+
 /// An unknown value in a logic problem.
 public struct Variable<Value> {
     /// A type-erased version of the variable.
-    internal var erased = AnyVariable()
+    internal var erased: AnyVariable
+    
+    /// The bijection information if this variable was created with `bimap`.
+    internal let bijection: Bijection?
     
     /// Create a new variable.
-    public init() { }
+    public init() {
+        self.init(AnyVariable())
+    }
+    
+    /// Create a new variable.
+    fileprivate init(_ erased: AnyVariable, bijection: Bijection? = nil) {
+        self.erased = erased
+        self.bijection = bijection
+    }
     
     public func map<NewValue>(_ transform: @escaping (Value) -> NewValue) -> Property<NewValue> {
         return Property<NewValue>(self, transform)
@@ -39,24 +89,17 @@ extension Variable: VariableProtocol {
     }
 }
 
-/// A class used to provide identity to `Variable`s.
-private class Identity { }
-
 /// A type-erased, hashable `Variable`.
-internal struct AnyVariable: Hashable {
-    fileprivate let identity: Identity
-    
-    /// Create a variable with an existing `Identity`.
-    fileprivate init(identity: Identity = Identity()) {
-        self.identity = identity
-    }
+internal class AnyVariable: Hashable {
+    /// Create a variable.
+    fileprivate init() { }
     
     var hashValue: Int {
-        return ObjectIdentifier(identity).hashValue
+        return ObjectIdentifier(self).hashValue
     }
     
     static func ==(lhs: AnyVariable, rhs: AnyVariable) -> Bool {
-        return lhs.identity === rhs.identity
+        return lhs === rhs
     }
 }
 
--- Tests/StateTests.swift
+++ Tests/StateTests.swift
@@ -9,6 +9,10 @@
 import XCTest
 @testable import Logician
 
+func + (lhs: Variable<Int>, rhs: Int) -> Variable<Int> {
+    return lhs.bimap(forward: { $0 + rhs }, backward: { $0 - rhs })
+}
+
 class StateTests: XCTestCase {
     // MARK: - `State.value(of: Property<Value>)`
     
@@ -22,6 +26,18 @@ class StateTests: XCTestCase {
         try! state.unify(x, "foo")
         
         XCTAssertEqual(state.value(of: y), 3)
+    }
+    
+    // MARK: - `State.value(of: Variable<Value>)`
+    
+    func testValueOfBimappedVariable() {
+        var state = State()
+        let x = Variable<Int>()
+        let y = x + 2
+        
+        try! state.unify(x, 2)
+        
+        XCTAssertEqual(state.value(of: y), 4)
     }
     
     // MARK: - `State.constrain()`
@@ -77,36 +93,123 @@ class StateTests: XCTestCase {
     func testUnifyVariableAndValue() {
         var state = State()
         let x = Variable<Int>()
+        
         try! state.unify(x, 4)
         try! state.unify(x, 4)
+        
         XCTAssertEqual(state.value(of: x), 4)
         XCTAssertThrowsError(try state.unify(x, 5))
+    }
+    
+    func testUnifyBimappedVariableAndValue() {
+        var state = State()
+        let x = Variable<Int>()
+        let y = x + 2
+        
+        try! state.unify(y, 4)
+        try! state.unify(y, 4)
+        
+        XCTAssertEqual(state.value(of: x), 2)
+        XCTAssertEqual(state.value(of: y), 4)
+        XCTAssertThrowsError(try state.unify(x, 5))
+        XCTAssertThrowsError(try state.unify(y, 5))
     }
     
     func testUnifyVariableAndVariableWithExistingValue() {
         var state = State()
         let x = Variable<Int>()
         let y = Variable<Int>()
+        
         try! state.unify(x, 4)
         try! state.unify(x, y)
+        
         XCTAssertEqual(state.value(of: y), 4)
+    }
+    
+    func testUnifyBimappedVariableAndVariableWithExistingValue() {
+        var state = State()
+        let x = Variable<Int>()
+        let y = Variable<Int>()
+        let z = y + 2
+        
+        try! state.unify(x, 4)
+        try! state.unify(x, z)
+        
+        XCTAssertEqual(state.value(of: x), 4)
+        XCTAssertEqual(state.value(of: y), 2)
+        XCTAssertEqual(state.value(of: z), 4)
+    }
+    
+    func testUnifyVariableAndBimappedVariableWithExistingValue() {
+        var state = State()
+        let x = Variable<Int>()
+        let y = Variable<Int>()
+        let z = y + 2
+        
+        try! state.unify(y, 2)
+        try! state.unify(x, z)
+        
+        XCTAssertEqual(state.value(of: x), 4)
+        XCTAssertEqual(state.value(of: y), 2)
+        XCTAssertEqual(state.value(of: z), 4)
     }
     
     func testUnifyVariableAndVariableWithNoExistingValue() {
         var state = State()
         let x = Variable<Int>()
         let y = Variable<Int>()
+        
         try! state.unify(x, y)
         try! state.unify(x, 4)
+        
         XCTAssertEqual(state.value(of: y), 4)
+    }
+    
+    func testUnifyVariableAndBimappedVariableWithNoExistingValue() {
+        var state = State()
+        let x = Variable<Int>()
+        let y = Variable<Int>()
+        let z = y + 2
+        
+        try! state.unify(x, z)
+        try! state.unify(x, 4)
+        
+        XCTAssertEqual(state.value(of: x), 4)
+        XCTAssertEqual(state.value(of: y), 2)
+        XCTAssertEqual(state.value(of: z), 4)
     }
     
     func testUnifyVariableAndVariableWithConflictingValues() {
         var state = State()
         let x = Variable<Int>()
         let y = Variable<Int>()
+        
         try! state.unify(x, 4)
         try! state.unify(y, 5)
+        
         XCTAssertThrowsError(try state.unify(x, y))
+    }
+    
+    func testUnifyVariableAndBimappedVariableWithConflictingValues() {
+        var state = State()
+        let x = Variable<Int>()
+        let y = Variable<Int>()
+        let z = y + 2
+        
+        try! state.unify(z, 4)
+        try! state.unify(x, 5)
+        
+        XCTAssertThrowsError(try state.unify(x, y))
+    }
+    
+    func testUnifyBimappedVariablesWithConflictingValues() {
+        var state = State()
+        let x = Variable<Int>()
+        let y = x + 3
+        let z = x + 2
+        
+        try! state.unify(z, 4)
+        
+        XCTAssertThrowsError(try state.unify(y, 4))
     }
 }
