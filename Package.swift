0d0efe136ab2baa469b72b007455e542ce815927
Ferran Pujol Camins <ferranpujolcamins@gmail.com>
2019-03-22 22:35:18 +0100

    Add missing product to Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "Logician",
+    products: [
+        .library(name: "Logician", targets: ["Logician"]),
+    ],
     targets: [
         .target(name: "Logician", dependencies: [], path: "Sources"),
         .testTarget(name: "LogicianTests", dependencies: ["Logician"], path: "Tests"),
